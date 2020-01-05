a311ad670a153ef772fa3244d2ad744c378af6eb
Sean M <53903852+shmccoy@users.noreply.github.com>
2020-01-05 03:58:46 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import Header from './Components/Header'
+import './App.css';
+import Parts from './Components/Parts';
+import Total from './Components/Total'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selected: {
+        Processor: {
+            name: '17th Generation Intel Core HB (7 Core with donut spare)',
+            cost: 700
+          },
+        "Operating System": {
+            name: 'Ubuntu Linux 16.04',
+            cost: 200
+          },
+        "Video Card":{
+            name: 'Toyota Corolla 1.5v',
+            cost: 1150.98
+          },
+        Display: {
+            name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+            cost: 1500
+          }
+      },
+      features: {
+        Processor: [
+          {
+            name: '17th Generation Intel Core HB (7 Core with donut spare)',
+            cost: 700
+          },
+          {
+            name: 'Professor X AMD Fire Breather with sidewinder technology',
+            cost: 1200
+          }
+        ],
+        "Operating System": [
+          {
+            name: 'Ubuntu Linux 16.04',
+            cost: 200
+          },
+          {
+            name: 'Bodhi Linux',
+            cost: 300
+          }
+        ],
+        "Video Card": [
+          {
+            name: 'Toyota Corolla 1.5v',
+            cost: 1150.98
+          },
+          {
+            name: 'Mind mild breeze 2000',
+            cost: 1345
+          }
+        ],
+        Display: [
+          {
+            name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+            cost: 1500
+          },
+          {
+            name: '15.3" HGTV (3840 x 2160) Home makeover edition',
+            cost: 1400
+          },
+        ]
+      }  
+    }
+  }
+
+//this will be used with other files
+  updateFeature(feature, newValue) {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />   
+        <main>
+        
+          
+        <Parts updateFeature={((feature, newValue) =>  this.updateFeature(feature, newValue))} selected={this.state.selected} features={this.state.features}/>
+
+         <Total selected={this.state.selected}/>
+        </main>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
